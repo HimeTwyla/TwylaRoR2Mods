fd19878b2ca43af274dbd9ac0e2f379ad83fe173
HimeTwyla <49589553+HimeTwyla@users.noreply.github.com>
2019-04-17 21:10:57 +0200

    Add files via upload

---
+++ InfiniteChance/InfiniteChance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace TwylaInfiniteChance
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Twyla.InfiniteChance", "InfiniteChance", "1.0.0")]
+    public class NoMoreLimits : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Chat.AddMessage("Infinite Chance loaded. Good luck gamers!");
+            On.RoR2.ShrineChanceBehavior.Awake += (orig, self) =>
+            {
+                orig(self);
+                self.maxPurchaseCount = 9000;
+            };
+        }
+    }
+}
