74627b65b4ca937e19209b73a614a57a3da20066
HimeTwyla <49589553+HimeTwyla@users.noreply.github.com>
2019-04-22 14:23:22 +0200

    Iz now IL master
    Removed the cooldown of shrines of chance

--- InfiniteChance/InfiniteChance.cs
+++ InfiniteChance/InfiniteChance.cs
@@ -4,13 +4,19 @@ using RoR2;
 using BepInEx.Configuration;
 using UnityEngine;
 using UnityEngine.Networking;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 
 namespace TwylaInfiniteChance
 {
+
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.Twyla.InfiniteChance", "InfiniteChance", "1.2.0")]
+    [BepInPlugin("com.Twyla.InfiniteChance", "InfiniteChance", "1.3.0")]
+
+
     public class NoMoreLimits : BaseUnityPlugin
     {
+
         public float CostMulti(string configline)
         {
             if (float.TryParse(configline, out float x))
@@ -20,15 +26,16 @@ namespace TwylaInfiniteChance
             return 0f;
         }
 
-
         private static ConfigWrapper<int> maxPurchase { get; set; }
         public static int maxPurchases { get { return NoMoreLimits.maxPurchase.Value; } protected set { NoMoreLimits.maxPurchase.Value = value; } }
 
 
+
         public void Awake()
         {
+            
             float CostMult = CostMulti(Config.Wrap("Money", "CostMultiplier", "By how much the cost will be multiplied after each purchase", "1.1").Value);
-            NoMoreLimits.maxPurchase = base.Config.Wrap<int>("Gamble", "Max Uses", "How many items you'll get before you start regretting your gambling addiction.", 9000);
+            NoMoreLimits.maxPurchase = base.Config.Wrap<int>("Gamble", "Max Uses", "How many items you'll get before you start regretting your gambling addiction.", 9000);                     
             Chat.AddMessage("Infinite Chance loaded. Good luck you sick bastards!");
             On.RoR2.ShrineChanceBehavior.Awake += (orig, self) =>
             {
@@ -36,6 +43,19 @@ namespace TwylaInfiniteChance
                 self.maxPurchaseCount = maxPurchases;
                 Harmony.AccessTools.Field(Harmony.AccessTools.TypeByName("RoR2.ShrineChanceBehavior"), "costMultiplierPerPurchase").SetValue(self, CostMult);
                 Chat.AddMessage(self.maxPurchaseCount.ToString());
+            };
+
+            On.EntityStates.Huntress.ArrowRain.OnEnter += (orig, self) =>
+            {
+                orig(self);
+                EntityStates.Huntress.ArrowRain.maxDuration = 200f;
+                Chat.AddMessage("Big ass rain of arrows inc");
+            };
+            IL.RoR2.ShrineChanceBehavior.AddShrineStack += il =>
+            {
+                var c = new ILCursor(il).Goto(144);
+                c.Remove();
+                c.Emit(OpCodes.Ldc_R4, 0f);
             };
         }
     }
