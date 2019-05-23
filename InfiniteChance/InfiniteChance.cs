2564d58f26a7df837c365f6e386aabd1c118ffb9
HimeTwyla <49589553+HimeTwyla@users.noreply.github.com>
2019-05-23 10:58:58 +0200

    Now even more spaghetti
    It's really not , I just had to fix some lib names and add the R2API reflection stuff

--- InfiniteChance/InfiniteChance.cs
+++ InfiniteChance/InfiniteChance.cs
@@ -5,20 +5,17 @@ using RoR2;
 using BepInEx.Configuration;
 using UnityEngine;
 using UnityEngine.Networking;
-using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using Mono.Cecil.Cil;
 using System.Collections;
+using R2API.Utils;
 
-namespace TwylaInfiniteChance
+namespace InfiniteChance2
 {
-
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.Twyla.InfiniteChance", "InfiniteChance", "1.3.2")]
-
-
-    public class NoMoreLimits : BaseUnityPlugin
+    [BepInPlugin("com.Twyla.InfiniteChance", "InfiniteChance", "1.4.0")]
+    public class InfiniteChance : BaseUnityPlugin
     {
-
         public float CostMulti(string configline)
         {
             try
@@ -33,21 +30,20 @@ namespace TwylaInfiniteChance
         }
 
         private static ConfigWrapper<int> maxPurchase { get; set; }
-        public static int maxPurchases { get { return NoMoreLimits.maxPurchase.Value; } protected set { NoMoreLimits.maxPurchase.Value = value; } }
+        public static int maxPurchases { get { return InfiniteChance.maxPurchase.Value; } protected set { InfiniteChance.maxPurchase.Value = value; } }
 
 
-
-        public void Awake()
+        public void Awake ()
         {
-
-            float CostMult = CostMulti(Config.Wrap("Money", "CostMultiplier", "By how much the cost will be multiplied after each purchase", "1.1").Value);
-            NoMoreLimits.maxPurchase = base.Config.Wrap<int>("Gamble", "Max Uses", "How many items you'll get before you start regretting your gambling addiction.", 9000);
+            float CostMult = CostMulti(Config.Wrap("Money", "CostMultiplier", "By how much the cost will be multiplied after each purchase", "1.2").Value);
+            InfiniteChance.maxPurchase = base.Config.Wrap<int>("Gamble", "Max Uses", "How many items you'll get before you start regretting your gambling addiction.", 9000);
             Chat.AddMessage("Infinite Chance loaded. Good luck you sick bastards!");
+
             On.RoR2.ShrineChanceBehavior.Awake += (orig, self) =>
             {
                 orig(self);
                 self.maxPurchaseCount = maxPurchases;
-                Harmony.AccessTools.Field(Harmony.AccessTools.TypeByName("RoR2.ShrineChanceBehavior"), "costMultiplierPerPurchase").SetValue(self, CostMult);
+                HarmonyLib.AccessTools.Field(HarmonyLib.AccessTools.TypeByName("RoR2.ShrineChanceBehavior"), "costMultiplierPerPurchase").SetValue(self, CostMult);
             };
 
             On.RoR2.ShrineChanceBehavior.AddShrineStack += (orig, self, interactor) =>
