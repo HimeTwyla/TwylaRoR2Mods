6d3fde8651aeea018d62104003d0bdcb2769be95
HimeTwyla <49589553+HimeTwyla@users.noreply.github.com>
2019-12-26 03:02:31 +0100

    Infinitechance 2.0
    More like "Fuck you Twyla , your checks seem to work but they don't"

--- InfiniteChance/InfiniteChance.cs
+++ InfiniteChance/InfiniteChance.cs
@@ -13,45 +13,99 @@ using R2API.Utils;
 namespace InfiniteChance2
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.Twyla.InfiniteChance", "InfiniteChance", "1.4.0")]
+    [BepInPlugin("com.Twyla.InfiniteChance", "InfiniteChance", "2.0.0")]
     public class InfiniteChance : BaseUnityPlugin
     {
-        public float CostMulti(string configline)
+        public ConfigEntry<bool> DisableCooldown { get; set; }
+        public ConfigEntry<bool> EnableIncreaseChance { get; private set; }
+        public float FailWeight { get; set; }
+        public float Failmult { get; set; }
+        public float CostMult { get; set; }
+        public int MaxPurchases { get; set; }
+        public int GuaranteedGreen { get; set; }
+        public int GuaranteedRed { get; set; }
+
+        int success = 0;
+        int fail = 0;
+        float RNGWeight;
+
+        public void Awake()
         {
-            try
-            {
-                float x = float.Parse(configline, CultureInfo.InvariantCulture);
-                return x;
-            }
-            catch (FormatException)
-            {
-                return 0.0f;
-            }
-        }
-
-        private static ConfigWrapper<int> maxPurchase { get; set; }
-        public static int maxPurchases { get { return InfiniteChance.maxPurchase.Value; } protected set { InfiniteChance.maxPurchase.Value = value; } }
-
-
-        public void Awake ()
-        {
-            float CostMult = CostMulti(Config.Wrap("Money", "CostMultiplier", "By how much the cost will be multiplied after each purchase", "1.2").Value);
-            InfiniteChance.maxPurchase = base.Config.Wrap<int>("Gamble", "Max Uses", "How many items you'll get before you start regretting your gambling addiction.", 9000);
-            Chat.AddMessage("Infinite Chance loaded. Good luck you sick bastards!");
-
+            GuaranteedGreen = Config.Bind<int>("Settings", "Guaranteed Green", 5, "The number of fails before you get a guaranteed green or red item. This setting needs enableincreasechance set to true").Value;
+            GuaranteedRed = Config.Bind<int>("Settings", "Guaranteed red", 10, "The number of fails before you get a guaranteed red item. This setting needs enableincreasechance set to true").Value;
+            FailWeight = Config.Bind<float>("Settings", "Fail weight", 10.1f, "Vanilla is 10.1 (101 = 100% fail). This setting needs enableincreasechance set to true").Value;
+            Failmult = Config.Bind<float>("Settings", "Fail multiplier", 0.95f, "Goes from 0 to 1 , the lower it goes the lower the chances to fail in a row. This setting needs enableincreasechance set to true").Value;
+            CostMult = Config.Bind<float>("Settings", "Cost Multiplier", 1.2f, "The cost multiplier, vanilla is 1.2").Value;
+            MaxPurchases = Config.Bind<int>("Settings", "Max Purchase count", 9000, "The number of item you'll get before the shrine turns off").Value;
+            DisableCooldown = base.Config.Bind<bool>("Toggles", "Enable cooldown removal", true, "Set to True or False , if set to True , will disable the cooldown on shrines. Vanilla :false");
+            EnableIncreaseChance = base.Config.Bind<bool>("Toggles", "Enable increase chance", true, "Set to True or False , if set to True shrines will get increasing chances on fail. Vanilla :false");
+            RNGWeight = FailWeight;
             On.RoR2.ShrineChanceBehavior.Awake += (orig, self) =>
             {
                 orig(self);
-                self.maxPurchaseCount = maxPurchases;
+                self.maxPurchaseCount = MaxPurchases;
                 HarmonyLib.AccessTools.Field(HarmonyLib.AccessTools.TypeByName("RoR2.ShrineChanceBehavior"), "costMultiplierPerPurchase").SetValue(self, CostMult);
             };
 
+
             On.RoR2.ShrineChanceBehavior.AddShrineStack += (orig, self, interactor) =>
             {
-                orig(self, interactor);
+                bool chance = EnableIncreaseChance.Value;
+                if (chance == true)
+                {
+                    var succ = self.GetFieldValue<int>("successfulPurchaseCount");
+                    if (succ == success)
+                    {
+                        FailWeight = (FailWeight * Failmult);
+                        ++fail;
+                        sendChatMessage("Fail " + fail + " weight " + FailWeight);
+                        if (fail >= GuaranteedRed) // this just doesn't fucking work
+                        {
+                            if (fail >= GuaranteedGreen && fail < GuaranteedRed)
+                            {
+                                sendChatMessage(fail + " fails in a row , guaranteed green " + FailWeight);
+                                FailWeight = (FailWeight * 0.5f);
+                                self.equipmentWeight = 0f;
+                                self.tier1Weight = 0f;
+                                self.tier2Weight = 0.8f;
+                                self.tier3Weight = 0.2f;
+                            }
+                            sendChatMessage(fail + " fails in a row , guaranteed red " + FailWeight);
+                            FailWeight = (FailWeight * 0f);
+                            self.equipmentWeight = 0f;
+                            self.tier1Weight = 0f;
+                            self.tier2Weight = 0f;
+                            self.tier3Weight = 1f; // None of it does
+                        }
+                    }
+                    if (succ != success)
+                    {
+                        ++success;
+                        FailWeight = RNGWeight;
+                        fail = 0;
+                        sendChatMessage("success " + "counter = " + success + "number of successes" + succ + "resetting fail weight" + FailWeight);
 
-                self.SetFieldValue("refreshTimer", 0f);
+                    }
+                }
+                bool CD = DisableCooldown.Value;
+                if (CD == true)
+                {
+                    orig(self, interactor);
+                    self.SetFieldValue("refreshTimer", 0f);
+                }
+                if (CD == false)
+                {
+                    orig(self, interactor);
+                    self.SetFieldValue("refreshTimer", 2f);
+                }
             };
+        }
+        private void sendChatMessage(string message)
+        {
+            Chat.SendBroadcastChat(new Chat.SimpleChatMessage
+            {
+                baseToken = message
+            });
         }
     }
 }
